c2ca2ffe55e3bd51afcfcdf6ca46efe27aee9172
raj3275 <pate3275@gmail.com>
2022-01-31 23:27:21 -0500

    Version 1

--- src/cardtrick/Card.java
+++ src/cardtrick/Card.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cardtrick;
 
 /**
@@ -11,7 +6,7 @@ package cardtrick;
  */
 public class Card 
 {
-    private int numbers; //encapsulate it getters and setters
+    private int numbers;
     private String suit;
     
     public static final String[] SUITS = {"diamonds","spades","clubs","hearts"};
--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -1,16 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Student Name: Raj Patel
+Student Number: 991637898
+*/
 package cardtrick;
 
-/** step 1: generate 7 random cards and store in array 
- * 52 deck size: 
- * 2 spades
- * 5 hearts
- * 1 clubs
- * ... 7 cards
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
  *
  * @author rajpa
  */
@@ -20,22 +17,37 @@ public class CardTrick {
      * @param args the command line arguments
      */
     public static void main(String[] args) 
-    {
+    {                
         
-        
-      Card[] magicHand = new Card[7]; //Array of object 
-      for (int i=0;i<magicHand.length;i++)
-      {
-        // step 1: generate 7 random cards and store in array
-          Card c= new Card();
-          c.setNumbers(2);//random generation for number/use method
-          c.setSuit("suit by random suit generation");
-          magicHand[i]=c; // stores the card object  in array
-        
-    }
-      //step 2: take input guess the card 2 spades find in array - print your card is found
-    
-    // step 3 do change in remote repository. hard coded card details card number =2,hearts to find in array
-    }
+        Card[] magicHand = new Card[7];
+        for (int i=0;i<magicHand.length;i++)
+        {        
+            Card c= new Card();
+            c.setNumbers((int)(1+Math.random()*13));
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i]=c;        
+        }
+
+        Scanner k = new Scanner(System.in);
+
+        System.out.println("Enter card number > 1 to 13");
+        int number = k.nextInt();
+        System.out.println("Enter card SUIT > spades, clubs, diamonds, hearts");
+        String suit = k.next();
+
+        int found = 0;
+        for(int i=0;i<magicHand.length;i++)
+        {
+            if (number==magicHand[i].getNumbers()){
+                if (suit.equals(magicHand[i].getSuit())){
+                    found = 1;
+                }
+            }
+        }
+        if(found==1)
+            System.out.println("Your Card is found!");
+        else
+            System.out.println("Your Card is not found!");
+      }
     
 }
